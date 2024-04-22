f569e2a591c13715fa375dddd56eb65297f3ae88
Laney McGlohon <lmcglohon@gmail.com>
2024-04-22 03:41:49 -0700

    More updates

---
+++ deleteTopContainer.py
@@ -0,0 +1,11 @@
+import json, requests, csv, authenticate
+
+baseURL, headers = authenticate.login()
+
+# Open file with top container uris
+f = open('top_containers_to_date.txt', 'r')
+
+# DELETE each top_container listed in the txt file
+for uri in f:
+  print(uri)
+  requests.delete(baseURL + uri, headers=headers)
